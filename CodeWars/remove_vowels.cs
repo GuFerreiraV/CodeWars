06c2cea4a3be1b15e7a3e533940c765e1f4d8516
user <user@DESKTOP-AGEOLGH>
2025-06-16 20:56:29 -0300

    Add project files.

---
+++ CodeWars/remove_vowels.cs
@@ -0,0 +1,13 @@
+﻿public class remove_vowels
+{
+    public static string Disemvowel(string str)
+    {
+        return new string(str.Where(c => !"aeiouAEIOU".Contains(c))
+            .ToArray());
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Disemvowel("This website is for losers LOL!"));
+    }
+}
