aca53481e7de5951fd3be6a7ea9b490c0a4aafc5
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-06-26 15:38:54 -0300

    Convert Bits - 7kyu

---
+++ CodeWars/DeltaBits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class DeltaBits
+    {
+        public static int ConvertBits(int a, int b)
+        {
+            int xor = a ^ b;
+            int count = 0;
+
+            while (xor > 0)
+            {
+                count++;
+                xor = xor & (xor - 1);
+            }
+            return count;
+        }
+
+        //public static void Main(string[] args) 
+        //{
+        //    Console.WriteLine(ConvertBits(31, 14));
+        //}
+    }
+}
