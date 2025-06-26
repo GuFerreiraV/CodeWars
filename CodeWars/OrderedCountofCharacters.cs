1808f9a7f59cb8d081c5810fbea3f7c0903a68ac
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-06-26 14:38:32 -0300

    Ordered Count of Characters - 7kyu

---
+++ CodeWars/OrderedCountofCharacters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class OrderedCountofCharacters
+    {
+        public static List<Tuple<char, int>> OrderedCount(string input)
+        {
+            return input
+                .GroupBy(c => c)
+                .Select(g => new Tuple<char, int>(g.Key, g.Count()))
+                .OrderBy(t => input.IndexOf(t.Item1))
+                .ToList();
+        }
+    }
+}
