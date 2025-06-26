d2ae6569445ec51d0a4d009fadf1f8f04204b063
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-06-26 15:08:56 -0300

    Reverse words - 7kyu

---
+++ CodeWars/ReverseWords.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class ReverseWords
+    {
+        public static string Reverse(string str) => string.Join(" ", str.Split(' ').Select(word => new string(word.Reverse().ToArray())));
+
+    }
+}
