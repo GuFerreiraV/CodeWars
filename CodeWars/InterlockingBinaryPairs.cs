ddfa8c20fc21d06611bfbbd8356da80ca7624f82
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-01 17:45:47 -0300

    Interlocking Binary Pairs - 7kyu

---
+++ CodeWars/InterlockingBinaryPairs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class InterlockingBinaryPairs
+    {
+            public static bool Interlockable(ulong a, ulong b)
+            {
+                return (a & b) == 0;
+            }
+        }
+    }
+
