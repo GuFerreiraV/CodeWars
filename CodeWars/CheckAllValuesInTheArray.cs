3b8458c84e96b15bf280160f9067e2403fde6921
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-01 17:45:24 -0300

    Find all occurrences of an element in an array - 7kyu

---
+++ CodeWars/CheckAllValuesInTheArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class CheckAllValuesInTheArray
+    {
+        public static int[] FindAll(int[] array, int n)
+        {
+            List<int> Found = new List<int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == n)
+                {
+                    Found.Add(i);
+                }
+            }
+            return Found.ToArray();
+        }
+    }
+}
+
