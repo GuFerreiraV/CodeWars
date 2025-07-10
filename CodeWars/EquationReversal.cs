6aee3f85f12df460c47549a220ff6be635781708
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-10 12:23:09 -0300

    Equation reversal - 7kyu

---
+++ CodeWars/EquationReversal.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class EquationReversal
+    {
+        public static string solve(string eq) {
+        
+           string pattern = @"(\d+)|([a-zA-Z])|([*/+-])";
+
+            MatchCollection matches = Regex.Matches(eq, pattern);
+            List<string> tokens = new List<string>();
+
+            foreach (Match match in matches) 
+            {
+                tokens.Add(match.Value);
+            }
+
+            tokens.Reverse();
+
+            return string.Join("", tokens);
+        }
+    }
+}
