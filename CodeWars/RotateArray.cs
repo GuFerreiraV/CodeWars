5aab135478f4698abba47afd79cead8b0b78360c
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-17 15:27:56 -0300

    Rotate Array (JS) - 6kyu

---
+++ CodeWars/RotateArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class RotateArray
+    {
+        public static object[] Rotate(object[] array, int n)
+        {
+            int length = array.Length;
+            int rotation = n % length;
+
+            if (rotation == 0)
+            {
+                return (object[])array.Clone();
+            }
+            ;
+            if (rotation < 0)
+            {
+                rotation += length;
+            }
+            if (array == null || array.Length <= 1)
+            {
+                return array;
+            }
+            int cort = length - rotation;
+            var final = array.Skip(cort);
+            var inicial = array.Take(cort);
+
+            return final.Concat(inicial).ToArray();
+        }
+    }
+}
