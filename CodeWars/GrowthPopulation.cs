0f5ca264a19916fddc88e7cbe0306d72962c059e
gustavo.vargens <gustavo.vargens@prodatamobility.com.br>
2025-12-05 13:16:41 -0300

    O(log n)

---
+++ CodeWars/GrowthPopulation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class GrowthPopulation
+    {
+        public static int NbYear (int p0, double percent, int aug, int p)
+        {
+            int years = 0;
+            while (p0 < p)
+            {
+                double result = p0 + (p0 * percent / 100) + aug;
+                p0 = (int)result;
+                years++;
+            }
+            return years;
+        } // O (log n)
+    }
+}
