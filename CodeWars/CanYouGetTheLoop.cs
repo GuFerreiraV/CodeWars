d49f4d438fe406bdb40a3ff9701c6fd5789af751
GuFerreiraV <gustavoferreiravargens@gmail.com>
2026-01-16 09:17:08 -0300

    Can you get the loop ? - 5 kyu - O(n)

---
+++ CodeWars/CanYouGetTheLoop.cs
@@ -0,0 +1,42 @@
+﻿/// <summary>
+/// Can you get the loop ? - 5 kyu - O(n)
+/// You are given a node that is the beginning of a linked list. 
+/// This list contains a dangling piece and a loop. Your objective is to determine the length of the loop. 
+/// 
+/// </summary>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class CanYouGetTheLoop
+    {
+        // Solução do Kata, A classe utilitária LoopDetector não foi disponibilizada, então comentei a função.
+
+        //public static int getLoopSize (LoopDetector.Node startNode) 
+        //{
+        //    LoopDetector.Node tartaruga = startNode;
+        //    LoopDetector.Node lebre = startNode;
+        //    int count = 0;
+        //    do
+        //    {
+        //        tartaruga = tartaruga.next;
+        //        lebre = lebre.next.next;
+        //    } while (tartaruga != lebre);
+
+        //    do
+        //    {
+        //        tartaruga = tartaruga.next;
+        //        count++;
+        //    } while (tartaruga != lebre);
+
+        //    return count;
+
+        //}
+
+    }
+
+}
