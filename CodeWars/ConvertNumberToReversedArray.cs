e78c33bffc5f4a930aee97fa1dea902772564303
GuFerreiraV <gustavoferreiravargens@gmail.com>
2026-01-20 10:34:16 -0300

    Convert Number to reversed array of digits - 8 kyu - O(N)

---
+++ CodeWars/ConvertNumberToReversedArray.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// Convert Number to reversed array of digits - 8 kyu - O(N)
+/// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+/// 35231 => [1,3,2,5,3]
+/// </summary>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class ConvertNumberToReversedArray
+    {
+        public static long[] Digitize (long n)
+        {            
+            return n
+                .ToString()
+                .Reverse()
+                .Select(c => (long)char.GetNumericValue(c))
+                .ToArray();
+        }
+    }
+}
