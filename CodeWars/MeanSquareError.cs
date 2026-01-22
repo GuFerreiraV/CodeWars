05a9d08e98dd94ff1f79c3865a0a208d26ea8878
GuFerreiraV <gustavoferreiravargens@gmail.com>
2026-01-22 10:47:12 -0300

    Mean Square Error -  5 kyu - O(N)

---
+++ CodeWars/MeanSquareError.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// Mean Square Error - 5 kyu - 
+/// Complete the function that
+
+/// accepts two integer arrays of equal length
+/// compares the value each member in one array to the corresponding member in the other
+/// squares the absolute value difference between those two values
+/// and returns the average of those squared absolute value difference between each member pair.
+/// </summary>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class MeanSquareError
+    {
+        public static double Solution (int[] firstArray, int[] secondArray)
+        {
+            return firstArray.Zip(secondArray, (a, b) => Math.Pow(a - b, 2)).Sum() / firstArray.Length;
+        }
+    }
+}
